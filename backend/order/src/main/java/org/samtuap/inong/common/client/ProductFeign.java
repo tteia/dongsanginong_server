8d87db2c951317749f950879957db02a2c51a1f7
H-Yeji <yeji980603@naver.com>
2024-10-29 09:30:52 +0900

    setting: yml 변경

--- backend/order/src/main/java/org/samtuap/inong/common/client/ProductFeign.java
+++ backend/order/src/main/java/org/samtuap/inong/common/client/ProductFeign.java
@@ -1,11 +1,9 @@
 package org.samtuap.inong.common.client;
 
-import org.samtuap.inong.config.FeignConfig;
 import org.samtuap.inong.domain.delivery.dto.FarmDetailGetResponse;
 import org.samtuap.inong.domain.coupon.dto.FarmSellerResponse;
 import org.samtuap.inong.domain.delivery.dto.PackageProductResponse;
 import org.samtuap.inong.domain.order.dto.PackageStatisticResponse;
-import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "product-service", configuration = FeignConfig.class)
 public interface ProductFeign {
 
     @GetMapping(value = "/product/info/{id}")
