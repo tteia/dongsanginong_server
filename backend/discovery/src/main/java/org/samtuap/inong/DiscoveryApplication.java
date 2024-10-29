7f1ea95da82514b875bbd8cf15b402b9d6cbf7ed
H-Yeji <yeji980603@naver.com>
2024-10-29 16:07:48 +0900

    chore

--- backend/discovery/src/main/java/org/samtuap/inong/DiscoveryApplication.java
+++ backend/discovery/src/main/java/org/samtuap/inong/DiscoveryApplication.java
@@ -3,12 +3,12 @@ package org.samtuap.inong;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
-//@EnableEurekaServer
+@EnableEurekaServer
 public class DiscoveryApplication {
 
 	public static void main(String[] args) {
