c5d0e3ff1308a933fd49c83d1eb96cca71669371
H-Yeji <yeji980603@naver.com>
2024-10-31 14:26:24 +0900

    chore: orderfeign의 오타 수정

--- backend/product/src/main/java/org/samtuap/inong/common/client/OrderFeignProd.java
+++ backend/product/src/main/java/org/samtuap/inong/common/client/OrderFeignProd.java
@@ -5,6 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Profile;
 
 @Profile("prod")
-@FeignClient(name = "order-service", url="http://product-service", configuration = FeignConfig.class)
+@FeignClient(name = "order-service", url="http://order-service", configuration = FeignConfig.class)
 public interface OrderFeignProd extends OrderFeign {
 }
